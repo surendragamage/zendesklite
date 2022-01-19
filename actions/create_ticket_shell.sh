ad613fc1af8eaf8f99665abf78afcfef0073e608
Surendra <amuvitag@adobe.com>
2022-01-19 21:09:11 +1100

    add more fields

--- actions/create_ticket_shell.sh
+++ actions/create_ticket_shell.sh
@@ -4,10 +4,11 @@
 
 SUBJ=$1
 DESC=$2
-STATUS=$1
-TYPE=$2
+STATUS=$3
+TYPE=$4
+PRIORITY=$5
 
-TICKET='{"ticket": {"subject": "'$SUBJ'","description": "'$DESC'","status":"'$STATUS'","type":"'$TYPE'"}}'
+TICKET='{"ticket": {"subject": "'$SUBJ'","description": "'$DESC'","status":"'$STATUS'","type":"'$TYPE'","priority":"'$PRIORITY'"}}'
 ENDPOINT="https://$SUBDOMAIN.zendesk.com/api/v2/tickets"
 CONTENT_TYPE="Content-Type:application/json"
 
